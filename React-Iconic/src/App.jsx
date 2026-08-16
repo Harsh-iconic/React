ff6533e2e93c62211a96098e30e7dcf154a3bc53
Harsh-iconic <harshsoalnki4@gmail.com>
2026-08-16 15:36:41 +0530

    react

---
+++ React-Iconic/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from './components/card'
+import Navbar from './components/navbar'
+const App = () => {
+  return (
+    <div>
+      <div>
+          <Navbar />
+          <Navbar />
+          <Card />
+          <Card />
+      </div>
+    </div>
+  )
+}
+
+export default App
+
---
+++ React-Iconic/src/components/card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const card = () => {
+  return (
+   <div className='card'>
+          <h1>Harsh</h1>
+          <p>Lorem ispum dolor sit amet constrectuer adipisicing elit</p>
+   </div>
+  )
+}
+
+export default card
